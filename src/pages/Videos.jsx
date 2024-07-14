4d113609761b428e526650808ebcad9c448906f4
iNa. <aljard@naver.com>
2024-07-14 21:36:57 +0900

    240714_Youtube Project (Header)

---
+++ src/pages/Videos.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Videos() {
+  const {keyword} = useParams();
+  return (
+    <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
+  )
+}
