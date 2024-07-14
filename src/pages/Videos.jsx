7b65a3409b6a1b707e319c220ecd3d6ad9eed371
iNa. <aljard@naver.com>
2024-07-14 22:06:00 +0900

    240714_Youtube Project (Mock data)

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,9 +1,43 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import VideoCard from '../components/VideoCard';
 
 export default function Videos() {
   const {keyword} = useParams();
+
+  // const {
+  //   isLoading, 
+  //   error, 
+  //   data: videos,
+  // } = useQuery(['videos', keyword], async () => {
+  //   return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+  //   .then((res) => res.json())
+  //   .then((data) => data.items);
+  // });
+
+  const queryFn = async () => {
+    return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+      .then((res) => res.json())
+      .then((data) => data.items);
+  };
+
+  const { isLoading, error, data: videos } = useQuery({
+    queryKey: ['videos', keyword],queryFn
+  })
+  
   return (
-    <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
   )
 }
