1f6458768ce8853260389e279c09793a32967497
iNa. <aljard@naver.com>
2024-07-16 18:42:00 +0900

    240716_Youtube Project (styling)

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -13,16 +13,16 @@ export default function Videos() {
     data: videos,
   } = useQuery({
     queryKey:['videos', keyword], 
-    queryFn:() => youtube.search(keyword)
+    queryFn:() => youtube.search(keyword),
+    staleTime: 1000 * 60 * 1
   });
   
   return (
     <>
-      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong</p>}
       {videos && (
-        <ul>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
           {videos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
